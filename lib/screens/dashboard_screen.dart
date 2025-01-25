6dab18725911511d81b6998cce23494fe782d201
Abid <abidroid1@gmail.com>
2025-01-25 13:24:09 +0500

    db completed

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,8 +1,12 @@
 import 'package:akti3_firebase_app/screens/add_task_screen.dart';
 import 'package:akti3_firebase_app/screens/login_screen.dart';
 import 'package:akti3_firebase_app/screens/profile_screen.dart';
+import 'package:akti3_firebase_app/screens/update_task_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:intl/intl.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -12,14 +16,32 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  CollectionReference? collectionReference;
+
+  @override
+  void initState() {
+    super.initState();
+
+    String uid = FirebaseAuth.instance.currentUser!.uid;
+    collectionReference = FirebaseFirestore.instance
+        .collection('tasks')
+        .doc(uid)
+        .collection('tasks');
+  }
+
+  TextStyle myTextStyle = TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton.extended(
         backgroundColor: Colors.green,
         foregroundColor: Colors.white,
-        onPressed: (){
-          Navigator.of(context).push(MaterialPageRoute(builder: (context){
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) {
             return AddTaskScreen();
           }));
         },
@@ -31,23 +53,109 @@ class _DashboardScreenState extends State<DashboardScreen> {
         foregroundColor: Colors.white,
         title: const Text('Dashboard'),
         actions: [
-          IconButton(onPressed: (){
-            Navigator.of(context).push(MaterialPageRoute(builder: (context){
-              return ProfileScreen();
-            }));
-          }, icon: Icon(Icons.person)),
+          IconButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  return ProfileScreen();
+                }));
+              },
+              icon: Icon(Icons.person)),
+          IconButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
 
-          IconButton(onPressed: (){
-
-            FirebaseAuth.instance.signOut();
-
-            Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context){
-              return LoginScreen();
-            }));
-          }, icon: Icon(Icons.logout)),
+                Navigator.of(context)
+                    .pushReplacement(MaterialPageRoute(builder: (context) {
+                  return LoginScreen();
+                }));
+              },
+              icon: Icon(Icons.logout)),
         ],
       ),
-      body: const Placeholder(),
+      body: StreamBuilder(
+          stream: collectionReference!.snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              QuerySnapshot querySnapshot = snapshot.data as QuerySnapshot;
+              List<QueryDocumentSnapshot> docs = querySnapshot.docs;
+
+              if (docs.isEmpty) {
+                return Center(
+                  child: Text('No Tasks'),
+                );
+              } else {
+                return ListView.builder(
+                  itemCount: docs.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      color: Colors.amber,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.only(left: 18),
+                              // color: Colors.blue,
+                              child: Column(
+                                spacing: 10,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    docs[index]['taskName'],
+                                    style: myTextStyle,
+                                  ),
+                                  Text(
+                                    getHumanReadableDate(docs[index]['dt']),
+                                    style: myTextStyle.copyWith(
+                                      fontSize: 22,
+                                      fontStyle: FontStyle.italic,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              IconButton(
+                                  onPressed: () {
+                                    collectionReference!
+                                        .doc(docs[index]['taskId'])
+                                        .delete();
+                                  },
+                                  icon: Icon(Icons.delete)),
+                              IconButton(
+                                  onPressed: () {
+                                    Navigator.of(context).push(
+                                        MaterialPageRoute(builder: (context) {
+                                      return UpdateTaskScreen(
+                                          documentSnapshot: docs[index]);
+                                    }));
+                                  },
+                                  icon: Icon(Icons.edit)),
+                            ],
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                );
+              }
+            } else {
+              return Center(
+                child: SpinKitDualRing(color: Colors.green),
+              );
+            }
+          }),
     );
+  }
+
+  String getHumanReadableDate(int dt) {
+    DateFormat dateFormat = DateFormat('dd/MM/yy hh:mm a');
+    DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(dt);
+
+    String humanDate = dateFormat.format(dateTime);
+
+    return humanDate;
   }
 }
--- lib/screens/update_task_screen.dart
+++ lib/screens/update_task_screen.dart
@@ -1,13 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class UpdateTaskScreen extends StatefulWidget {
-  const UpdateTaskScreen({super.key});
+  const UpdateTaskScreen({super.key, required this.documentSnapshot});
+
+  final DocumentSnapshot documentSnapshot;
 
   @override
   State<UpdateTaskScreen> createState() => _UpdateTaskScreenState();
 }
 
 class _UpdateTaskScreenState extends State<UpdateTaskScreen> {
+
+  var taskController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    taskController.text = widget.documentSnapshot['taskName'];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +29,44 @@ class _UpdateTaskScreenState extends State<UpdateTaskScreen> {
         foregroundColor: Colors.white,
         title: const Text('Add Task'),
       ),
-      body: const Placeholder(),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: taskController,
+              decoration: InputDecoration(
+                hintText: 'Task Name',
+                border: OutlineInputBorder()
+              ),
+            ),
+            
+            ElevatedButton(onPressed: (){
+
+              String updatedTask = taskController.text;
+
+              String uid = FirebaseAuth.instance.currentUser!.uid;
+
+              // FirebaseFirestore.instance.collection('tasks')
+              // .doc(uid).collection('tasks')
+              // .doc(widget.documentSnapshot['taskId'])
+              // .update({
+              //   'taskName': updatedTask,
+              // });
+
+
+              widget.documentSnapshot.reference.update({
+                'taskName': updatedTask,
+                'dt': DateTime.now().millisecondsSinceEpoch
+              });
+
+
+
+
+            }, child: const Text("UPDATE"))
+          ],
+        ),
+      ),
     );
   }
 }
